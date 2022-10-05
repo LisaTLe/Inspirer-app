53eb8d340eee4473ed586abba9c9391f1631c842
LisaTLe <lisa.le1127@gmail.com>
2022-10-04 19:33:24 -0600

    working on colormindAPI and small edits

--- routes/colormindAPI.js
+++ routes/colormindAPI.js
@@ -1,82 +1,16 @@
-// const express = require("express");
-// const router = express.Router();
-// const axios = require("axios");
+const url = "http://colormind.io/api/";
+const data = {
+  model: "default",
+  //   input: [[44, 43, 44], [90, 83, 82], "N", "N", "N"],
+};
 
-// // Convert Hex to RGB functions, some calcs can be moved to utils
-// function hexToR(h) {
-//   return parseInt(cutHex(h).substring(0, 2), 16);
-// }
-// function hexToG(h) {
-//   return parseInt(cutHex(h).substring(2, 4), 16);
-// }
-// function hexToB(h) {
-//   return parseInt(cutHex(h).substring(4, 6), 16);
-// }
-// function cutHex(h) {
-//   return h.charAt(0) === "#" ? h.substring(1, 7) : h;
-// }
+const http = new XMLHttpRequest();
 
-// // Shuffle Array to slightly randomize palette result
-// function shuffle(array) {
-//   var currentIndex = array.length,
-//     temporaryValue,
-//     randomIndex;
-//   // While there remain elements to shuffle...
-//   while (0 !== currentIndex) {
-//     // Pick a remaining element...
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
-//     // And swap it with the current element.
-//     temporaryValue = array[currentIndex];
-//     array[currentIndex] = array[randomIndex];
-//     array[randomIndex] = temporaryValue;
-//   }
-//   return array;
-// }
+http.onreadystatechange = function () {
+  if (http.readyState == 4 && http.status == 200) {
+    const palette = JSON.parse(http.responseText).result;
+  }
+};
 
-// router.get("/", (req, res) => {
-//   // recieve query string and split
-//   let hexColors = req.query.colors.split(",");
-
-//   // convert to RGB from Hex
-//   let hexToRGB = [];
-//   for (let i = 0; i < hexColors.length; i++) {
-//     let R = hexToR(hexColors[i]);
-//     let G = hexToG(hexColors[i]);
-//     let B = hexToB(hexColors[i]);
-//     hexToRGB.push([R, G, B]);
-//   }
-//   // Shuffles which 5 colors are being sent
-//   hexToRGB = shuffle(hexToRGB);
-
-//   // if greater than length 5 slice
-//   if (hexToRGB.length > 5) {
-//     hexToRGB = hexToRGB.slice(0, 5);
-//     // add a 'N' empty slot
-//     // Api fills in a color if slot empty
-//     hexToRGB = hexToRGB.concat("N");
-//   }
-
-//   // if less than 5 push 'N' in remaining slots
-//   if (hexToRGB.length < 5) {
-//     for (let i = hexToRGB.length; i < 5; i++) {
-//       hexToRGB.push("N");
-//     }
-//   }
-
-//   //only send first 5 colors
-//   axios
-//     .post("http://colormind.io/api/", {
-//       model: "default",
-//       input: shuffle(hexToRGB).slice(0, 5)
-//     })
-//     .then(response => {
-//       res.json(response.data.result);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//       res.status(400).json({ msg: "Bad Request or Error from ColorMind" });
-//     });
-// });
-
-// module.exports = router;
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'; 
-import { PhotoCamera } from '@material-ui/icons';
-import Button from '@mui/material/Button';
-import { purple } from '@mui/material/colors';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import Button from "@mui/material/Button";
+import { purple } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Paper from "@mui/material/Paper";
 import FormLabel from "@mui/material/FormLabel";
 
-
-
-
-
-function App() { 
-  
+function App() {
   return (
-    
     <>
-<div color="accent">
-
-    
-    <Typography variant="h5" align="left" color="textSecondary" paragraph>
-  THIS IS APPLICATION TITLE
-</Typography>
-    <CssBaseline /> 
-    <AppBar position='relative'>
-      <Toolbar>
-        <PhotoCamera />
-        <Typography variant="h6">
-         NAV BAR
+      <div color="accent">
+        <Typography variant="h5" align="left" color="textSecondary" paragraph>
+          THIS IS APPLICATION TITLE
         </Typography>
-      </Toolbar>
-    </AppBar>
-    <main>
-      <div>
-        <Container maxWidth="sm">
-<Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-  ART
-</Typography>
-<Typography variant="h5" align="center" color="textSecondary" paragraph>
-  TEST TEST TEST TEST 
-</Typography>
-        </Container>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <PhotoCameraIcon />
+            <Typography variant="h6">NAV BAR</Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div>
+            <Container maxWidth="sm">
+              <Typography
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                ART
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="textSecondary"
+                paragraph
+              >
+                TEST TEST TEST TEST
+              </Typography>
+            </Container>
+          </div>
+        </main>
       </div>
-    </main>
-    </div>
     </>
   );
 }
