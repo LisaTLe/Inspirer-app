cce5d0b3fd9cafba564f314aeec129e84cc49018
LisaTLe <lisa.le1127@gmail.com>
2022-10-08 11:51:35 -0600

    create color generator and edit pwa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import Main from './components/main';
+import * as React from "react";
+import Main from "./components/main";
+import Colors from "./components/generator";
 // import logo from "./logo.svg";
 import "./App.css";
 
-
 function App() {
-  
   return (
-    <div>
-    <Main />
-      
+    <div id="root">
+      <Main />
+      <Colors />
     </div>
   );
 }
