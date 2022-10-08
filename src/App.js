b34be38bb7ce17a8c375b5a92b710d3b92e1e95b
vanessaroman <vanessavirginiarr@gmail.com>
2022-10-08 13:35:18 -0600

    art api

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import { search } from "./routes/chiArtApi";
 import Search from "./routes/search-art";
 import SearchResults from "./routes/searchresults-art";
+import { useState, useEffect } from "react";
 
 
 import {
@@ -34,11 +35,43 @@ import Tab from '@mui/material/Tab';
 
 
 function App() {
+
+  const [results, setResults] = useState(null);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Clear the results if the user gets rid of the search query
+    if (!(query || query.length)) {
+      setResults(null);
+      return;
+    }
+
+    // Don't search for anything less than 3 characters
+    if (query.length < 3) {
+      return;
+    }
+
+    // Start a search
+    setLoading(true);
+    search(query, "id", "title", "image_id", "thumbnail")
+      .then((searchResults) => {
+        if (searchResults && searchResults.data) {
+          setResults(searchResults.data);
+        }
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, [query]);
+
   return (
     <div id="root">
     <Navbar />
       <Main />
       <Colors />
+      <Search query={query} onChange={(e) => setQuery(e.target.value)} />
+      <SearchResults results={results} loading={loading} />
     </div>
   );
 }
