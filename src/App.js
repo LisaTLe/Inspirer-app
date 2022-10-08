f208e31945fa749744fe08c80e8c157ecbf0d7fa
Can Sardaslar <can.sardaslar@me.com>
2022-10-08 10:33:51 -0600

    main page from app.js moved to Components

--- src/App.js
+++ src/App.js
@@ -1,85 +1,16 @@
 import * as React from 'react';
-
+import Main from './components/main';
 // import logo from "./logo.svg";
 import "./App.css";
-import Search from "./routes/chiArtApi.js"
-
-import {
-  Typography,
-  AppBar,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  CssBaseline,
-  Grid,
-  Toolbar,
-  Container,
-} from "@material-ui/core";
-import Button from "@mui/material/Button";
-import { purple } from "@mui/material/colors";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import Paper from "@mui/material/Paper";
-import FormLabel from "@mui/material/FormLabel";
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 
 
 function App() {
   
   return (
-    <>
-    
-      <div color="accent">
-        <Typography variant="h5" align="left" color="textSecondary" paragraph>
-          THIS IS APPLICATION TITLE
-        </Typography>
-        <CssBaseline />
-        <AppBar position="relative">
-          <tabs align="center"> 
-          <Tab label="Generate"/>
-          <Tab label="Profile"/>
-          <Tab label="Explore Pinterest"/>
-          <Tab label="Shop"/>
-          
-          </tabs>
-          <Toolbar>
-            <Typography variant="h6"> </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <div>
-            <Container maxWidth="sm">
-              <Typography
-                variant="h2"
-                align="center"
-                color="textPrimary"
-                gutterBottom
-              >
-               
-
-                ART
-              </Typography>
-              </Container>
-              </div>
-
-              <div>
-              <Container>
-              <Typography
-                variant="h5"
-                align="center"
-                color="textSecondary"
-                paragraph
-              >
-                TEST TEST TEST TEST
-              </Typography>
-            </Container>
-          </div>
-        </main>
-      </div>
-    </>
+    <div>
+    <Main />
+      
+    </div>
   );
 }
 
