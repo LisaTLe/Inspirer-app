ae6b05e3496fe0104298e884997d0da20679f196
theybie <zoyasarow@gmail.com>
2022-10-09 15:48:10 -0600

    color changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,14 +14,21 @@ import {
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const TitleTypography = createTheme({
+  root: {
+    color: "8ba4b1"
+  }
+})(Typography);
 
 export default function navbar() {
   return (
     <>
-    <Box pt={3} pl={3} pb={3}>
-      <Typography variant="h5" align="left" color="textSecondary" paragraph>
+    <Box pt={3} pl={3} pb={3} >
+      <TitleTypography variant="h5" align="left" color="textSecondary" paragraph>
         INSPIRER: An Art & Design Generator
-      </Typography>
+      </TitleTypography>
     </Box>
       <CssBaseline />
       <AppBar position="relative">
@@ -39,28 +46,3 @@ export default function navbar() {
     </>
   );
 }
-
-// <div className="navbar">
-//   <div className="wrapper">
-//     <div className="left">
-//       <a href="#home" className="title">
-//         Name of application here
-//       </a>
-//       <a href="#home" className="home-nav">
-//         Home
-//       </a>
-//       <a href="#generate" className="generate-nav">
-//         Generate
-//       </a>
-//       <a href="#profile" className="profile-nav">
-//         Profile
-//       </a>
-//       <a href="#explore" className="explore-nav">
-//         Explore Pinterest
-//       </a>
-//       <a href="#shop" className="shop-nav">
-//         Shop
-//       </a>
-//     </div>
-//   </div>
-// </div>
