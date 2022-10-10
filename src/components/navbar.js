038e3b9dbb71f63ec524ce28f00c19cdf05bcbb1
LisaTLe <lisa.le1127@gmail.com>
2022-10-09 20:46:21 -0600

    work on all user inputs

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,33 +11,39 @@ import {
   Toolbar,
   Container,
 } from "@material-ui/core";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const TitleTypography = createTheme({
   root: {
-    color: "8ba4b1"
-  }
+    color: "8ba4b1",
+  },
 })(Typography);
 
 export default function navbar() {
   return (
     <>
-    <Box pt={3} pl={3} pb={3} >
-      <TitleTypography variant="h5" align="left" color="textSecondary" paragraph>
-        INSPIRER: An Art & Design Generator
-      </TitleTypography>
-    </Box>
+      <Box pt={3} pl={3} pb={3}>
+        <TitleTypography
+          variant="h5"
+          align="left"
+          color="textSecondary"
+          paragraph
+        >
+          INSPIRER: An Art & Design Generator
+        </TitleTypography>
+      </Box>
       <CssBaseline />
       <AppBar position="relative">
         <tabs align="center">
-          <Tab label="Generate" />
-          <Tab label="Profile" />
+          <Tab label="Home" to="/" />
+          <Tab label="Sign Up" to="/signup" />
+          <Tab label="Profile" to="/me" />
           <Tab label="Explore Pinterest" />
           <Tab label="Shop" />
-          <Tab label="Install" />
+          <Tab label="Install Now" />
         </tabs>
         <Toolbar>
           <Typography variant="h6"> </Typography>
