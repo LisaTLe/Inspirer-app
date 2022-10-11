24fad1589f7c7edcf2e995edde971da869c37247
LisaTLe <lisa.le1127@gmail.com>
2022-10-10 19:41:25 -0600

    small changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,35 +16,35 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const TitleTypography = createTheme({
-  root: {
-    color: "8ba4b1",
-  },
-})(Typography);
+// const TitleTypography = createTheme({
+//   root: {
+//     color: "8ba4b1",
+//   },
+// })(Typography);
 
 export default function navbar() {
   return (
     <>
       <Box pt={3} pl={3} pb={3}>
-        <TitleTypography
+        {/* <TitleTypography
           variant="h5"
           align="left"
           color="textSecondary"
           paragraph
-        >
-          INSPIRER: An Art & Design Generator
-        </TitleTypography>
+        > */}
+        INSPIRER: An Art & Design Generator
+        {/* </TitleTypography> */}
       </Box>
       <CssBaseline />
       <AppBar position="relative">
-        <tabs align="center">
+        <Tabs align="center">
           <Tab label="Home" to="/" />
           <Tab label="Sign Up" to="/signup" />
           <Tab label="Profile" to="/me" />
           <Tab label="Explore Pinterest" />
           <Tab label="Shop" />
           <Tab label="Install Now" />
-        </tabs>
+        </Tabs>
         <Toolbar>
           <Typography variant="h6"> </Typography>
         </Toolbar>
