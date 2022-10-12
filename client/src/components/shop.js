e9292b9f6b8455ee1aa44bcf04e25aa12c84a53e
vanessaroman <vanessavirginiarr@gmail.com>
2022-10-11 23:51:00 -0600

    shop link fix

--- client/src/components/shop.js
+++ client/src/components/shop.js
@@ -1,6 +1,5 @@
 import React,{useState} from 'react';
 
-
 function Shop() {
   const [state,setState]=useState(false);
   let url="https://shop.artic.edu/?gclid=CjwKCAjwqJSaBhBUEiwAg5W9p8e4yQROtBbJCQ6cce_AMI8sHYj_3aU3GAyvZ65ZlFDUMVBE53P75hoC-WUQAvD_BwE";
