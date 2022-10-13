a70591d3a53f8810ef6061d98db83ac75b36a7d7
LisaTLe <lisa.le1127@gmail.com>
2022-10-12 18:05:01 -0600

    small changes

--- client/src/components/profilePage/ReadOnlyRow.js
+++ client/src/components/profilePage/ReadOnlyRow.js
@@ -2,24 +2,22 @@ import React from "react";
 
 const ReadOnlyRow = ({ project, handleEditClick, handleDeleteClick }) => {
   return (
-    <div>
-      <tr>
-        <td>{project.projectName}</td>
-        <td>{project.colors}</td>
-        <td>{project.paintName}</td>
-        <td>
-          <button
-            type="button"
-            onClick={(event) => handleEditClick(event, project)}
-          >
-            Edit
-          </button>
-          <button type="button" onClick={() => handleDeleteClick(project.id)}>
-            Delete
-          </button>
-        </td>
-      </tr>
-    </div>
+    <tr>
+      <td>{project.projectName}</td>
+      <td>{project.colors}</td>
+      <td>{project.paintName}</td>
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, project)}
+        >
+          Edit
+        </button>
+        <button type="button" onClick={() => handleDeleteClick(project.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
   );
 };
 
